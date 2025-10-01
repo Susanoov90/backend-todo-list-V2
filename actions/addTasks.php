7b701aee2ceeb83040deb18e38a834d4eed19ad5
KOUAKOU Georgio <georgino.kouak@gmail.com>
2025-10-01 14:18:49 -0400

    Update logic of addTask

--- actions/addTasks.php
+++ actions/addTasks.php
@@ -1,8 +1,8 @@
 <?php
-    require("connectToDB.php");
+require("connectToDB.php");
 
 // Vérifier si le formulaire a été soumis
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["pushSubmit"])) {
     // Récupérer les données du formulaire
     $nom = $_POST['nom'];
     $description = $_POST['description'];
@@ -10,13 +10,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Préparer la requête d'insertion avec des requêtes préparées pour la sécurité
     // Cela empêche les injections SQL
     $sql = "INSERT INTO task (nom, description) VALUES (?, ?)";
-    $stmt= $conn->prepare($sql);
+    $stmt = $conn->prepare($sql);
 
     // Lier les valeurs et exécuter la requête
     $stmt->execute([$nom, $description]);
-
-    echo "Données ajoutées avec succès !";
-} else {
-    echo "Erreur : Méthode de requête invalide.";
+    
+    //Si l'exécution a marché, la on affiche le succès ou l'échec
+    if($stmt == true || $stmt == 1){
+        echo "Données ajoutées avec succès !";
+    } else {
+        echo "Erreur : Méthode de requête invalide.";
+    }
 }
-?>
