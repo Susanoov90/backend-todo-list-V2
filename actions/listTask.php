3099126a0ce49f8b6670fcdf864d25e56f097929
KOUAKOU Georgio <georgino.kouak@gmail.com>
2025-10-01 14:56:38 -0400

    Add listTasks.php

---
+++ actions/listTask.php
@@ -0,0 +1,20 @@
+<?php
+    require("connectToDB.php");
+
+    //Code SQL à executer
+    $sql = $conn->prepare("SELECT * FROM task");
+    $sql->execute();
+
+    //On recupere les données qu'on met dans un tableau
+    $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+    //On recupere le nombre d'element du tableau
+    $length = count($rows);
+
+    //Si c'est inferieur, c'est que ya rien oh
+    if($length < 1){
+        echo "Aucune donnée";
+    } else {
+        echo "Ya données oh";
+    }
+
