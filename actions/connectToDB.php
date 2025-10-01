058a8be74580220e83d07fa6a84f8de9bddab9f8
KOUAKOU Georgio <georgino.kouak@gmail.com>
2025-10-01 14:19:17 -0400

    Fix displayed text from the connexion file

--- actions/connectToDB.php
+++ actions/connectToDB.php
@@ -8,8 +8,9 @@ try {
    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
    // set the PDO error mode to exception
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Connected successfully";
+   echo "<script>console.log('Connect success')</script>"; //Je veux que le message de succes ou d'echec au niveau de la connexion à la BD s'affiche dans la console
 } catch(PDOException $e) {
-   echo "Connection failed: " . $e->getMessage();
+   $message = "Connection failed: " . $e->getMessage();
+   echo "<script> console.log("+ $message  +")</script>"; //Meme chose ici
 }
 ?>
